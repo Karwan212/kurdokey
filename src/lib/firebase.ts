ed9922851be0a6a47af038cdfdbbf24c32f8562d
Karwan212 <karwanbarzngy@gmail.com>
2026-03-07 10:47:13 +0300

    Add files via upload

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getAnalytics, isSupported } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "",
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "",
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "",
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "",
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || "",
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyCwfbsK4Hve_IqIVUNiOSAxKlbYCBYE8Vk",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "kurdokey-d1889.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "kurdokey-d1889",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "kurdokey-d1889.firebasestorage.app",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "792333169296",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:792333169296:web:69edb6b4d658a633461e15",
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "G-7CYCVD43R9",
 };
 
 // Only initialize if we have at least an API key
